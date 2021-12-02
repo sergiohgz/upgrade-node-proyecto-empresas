622c530d2c9ffa7266c7583ddfa9a3de79fe3721
Sergio Herrera <herrera.guzman@bootcamp-upgrade.com>
2021-12-02 20:24:26 +0100

    Proyecto empresa finalizado

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv'); // require('dotenv').config();
 dotenv.config();
-console.log('ENTORNO:', process.env);
+
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
 const SECRET_SESSION = process.env.SECRET_SESSION;
